d30290d01c611c66fc13a96a0cf584c5ffa96be5
Jessica Luz <jessuca.luz@matera.com>
2020-01-07 16:38:30 -0300

    Initialing Cucumber Course

---
+++ src/test/java/LearnCucumber.java
@@ -0,0 +1,27 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+public class LearnCucumber {
+	
+	@Given("^that I created the file correctly$")
+	public void that_I_created_the_file_correctly() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I execute this$")
+	public void i_execute_this() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the specification should finish succesfully$")
+	public void the_specification_should_work_succesfully() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
---
+++ target/test-classes/learn_cucumber.feature
@@ -0,0 +1,10 @@
+Feature: Learn Cucumber
+	As a student
+	I want to learn how to use Cucumber
+	So I can automate acceptance scenarios
+
+Scenario: Should execute specification
+
+  Given that I created the file correctly
+  When I execute this 
+  Then the specification should finish succesfully
