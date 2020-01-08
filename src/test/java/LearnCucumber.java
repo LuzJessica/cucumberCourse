7c6fe958b970e4c38d778d3f172f7493ed86bca2
Jessica Luz <jessuca.luz@matera.com>
2020-01-08 12:26:32 -0300

    Using asserts to validate tests

--- src/test/java/LearnCucumber.java
+++ src/test/java/LearnCucumber.java
@@ -1,4 +1,6 @@
 
+import org.junit.Assert;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -22,6 +24,8 @@ public class LearnCucumber {
 	}
 	
 	private int count = 0;
+	private int division = 0;
+	private int multiplication = 0;
 	@Given("^that counter value is (\\d+)$")
 	public void thatCounterValueIs(int arg1) throws Throwable {
 	      count = arg1;   
@@ -32,13 +36,41 @@ public class LearnCucumber {
 	public void iIncrementWith(int arg1) throws Throwable {
 	    count += arg1;
 	}
+	
 
-	@Then("^the counter value will be (\\d+)$")
-	public void theCounterValueWillBe(int arg1) throws Throwable {
-		System.out.println(count);
-		System.out.println(arg1);
+	@Then("^the counter incremented value will be (\\d+)$")
+	public void theCounterIncrementedValueWillBe(int arg1) throws Throwable {
+		Assert.assertFalse(arg1==count);
 	}
 	
+	@When("^I decrement with (\\d+)$")
+	public void iDecrementWith(int arg1) throws Throwable {
+	    count -= arg1;
+	}
 	
+	@Then("^the counter decremented value will be (\\d+)$")
+	public void theCounterDecrementedValueWillBe(int arg1) throws Throwable {
+		Assert.assertTrue(arg1==10);
+	}
+	
+	@When("^I divide by (\\d+)$")
+	public void iDivideBy(int arg1) throws Throwable {
+	    division = count/arg1;
+	}
+	
+	@Then("^the counter divided by value will be (\\d+)$")
+	public void theCounterDividedByValueWillBe(int arg1) throws Throwable {
+	   Assert.assertEquals(arg1, division);
+	}
+	
+	@When("^I multiplicate by (\\d+)$")
+	public void iMultiplicateBy(int arg1) throws Throwable {
+		multiplication = count * arg1;
+	}
+
+	@Then("^the counter multiplicated by value will be (\\d+)$")
+	public void theCounterMultiplicatedByValueWillBe(int arg1) throws Throwable {
+		Assert.assertNotEquals(arg1, multiplication);
+	}
 
 }
--- src/test/java/learn_cucumber.feature
+++ src/test/java/learn_cucumber.feature
@@ -13,4 +13,17 @@ Scenario: Should increment counter
 
 	Given that counter value is 15
 	When I increment with 5
-	Then the counter value will be 18
+	When I decrement with 5
+	When I divide by 5
+	When I multiplicate by 2
+	Then the counter incremented value will be 18
+	Then the counter decremented value will be 10
+	Then the counter divided by value will be 3
+	Then the counter multiplicated by value will be 90
+	
+
+	
+	
+
+
+ 
