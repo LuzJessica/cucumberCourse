7f70f8e7f8c46c4d1d84d290989a8159de7dc698
Jessica Luz <jessuca.luz@matera.com>
2020-01-08 10:48:12 -0300

    Learning about Cucumber Options as plugin, monochrome, snippets, dryRun, strict

--- src/test/java/LearnCucumber.java
+++ src/test/java/LearnCucumber.java
@@ -7,17 +7,17 @@ import cucumber.api.java.en.When;
 public class LearnCucumber {
 	
 	@Given("^that I created the file correctly$")
-	public void that_I_created_the_file_correctly() throws Throwable {
+	public void thatICreatedTheFileCorrectly() throws Throwable {
 	    
 	}
 
 	@When("^I execute this$")
-	public void i_execute_this() throws Throwable {
+	public void iExecuteThis() throws Throwable {
 	    
 	}
 
 	@Then("^the specification should finish succesfully$")
-	public void the_specification_should_work_succesfully() throws Throwable {
+	public void theSpecificationShouldFinishSuccesfully() throws Throwable {
 	    
 	}
 
--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,9 +1,18 @@
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = "pretty",
+		monochrome = true,
+		snippets = SnippetType.CAMELCASE,
+		dryRun = true,
+		strict = true)
+		
 public class Runner {
 	
 	
